2586a5abddf7832fd3172667929d68f6952ac30d
philippezwietering <philippe.zwietering@gmail.com>
2017-10-02 10:18:43 +0200

    Meer functionality in python part and added rs232 library

--- pc_python/lemonator.cpp
+++ pc_python/lemonator.cpp
@@ -20,6 +20,35 @@ PYBIND11_MODULE( lemonator, m ) {
    
    py::class_< lemonator_proxy >( m, "lemonator" )
       .def( py::init< int >() )
-      .def_readonly( "led_yellow", &lemonator_proxy::p_led_yellow );
+      .def_readonly( "led_yellow", &lemonator_proxy::p_led_yellow )
+      .def_readwrite( "port", &lemonator_proxy::port )
+      .def_readwrite( "lcd", &lemonator_proxy::p_lcd )
+      .def_readwrite( "keypad", &lemonator_proxy::p_keypad)
+      .def_readwrite( "distance", &lemonator_proxy::p_distance)
+      .def_readwrite( "color", &lemonator_proxy::p_color)
+      .def_readwrite( "temperature", &lemonator_proxy::p_temperature)
+      .def_readwrite( "reflex", &lemonator_proxy::p_reflex)
+      .def_readwrite( "heater", &lemonator_proxy::p_heater)
+      .def_readwrite( "sirup_pump", &lemonator_proxy::p_sirup_pump)
+      .def_readwrite( "sirup_valve", &lemonator_proxy::p_sirup_valve)
+      .def_readwrite( "water_pump", &lemonator_proxy::p_water_pump)
+      .def_readwrite( "water_valve", &lemonator_proxy::p_water_valve)
+      .def_readonly( "led_green", &lemonator_proxy::p_led_green);
+
+   py::class_< sensor_proxy >(m, "sensor_proxy")
+      .def( "read_mc", &sensor_proxy::read_mc )
+      .def( "read_mm", &sensor_proxy::read_mm )
+      .def( "read_rgb", &sensor_proxy::read_rgb )
+      .def( "getc", &sensor_proxy::getc )
+      .def( "get", &sensor_proxy::get, "", py::arg("buf") = hwlib::buffering::unbuffered );
+
+   py::class_< lcd_proxy >(m, "lcd_proxy")
+      .def( "putc", &lcd_proxy::putc, "", py::arg("c") );
+
+   py::class_< serial_port>(m, "serial_port")
+      .def( "read", &serial_port::read )
+      .def( "write", &serial_port::write, "", py::arg("s"))
+      .def( "clear", &serial_port::clear )
+      .def( "transaction", &serial_port::transaction, "", py::arg("s"), py::arg("response") = true );
 }
 
