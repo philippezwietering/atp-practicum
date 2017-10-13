62e12c52e1e1c10230078ef2c85f558d72565123
philippezwietering <philippe.zwietering@gmail.com>
2017-10-13 10:27:01 +0200

    Made printing to lcd better

--- pc_python/Controller.py
+++ pc_python/Controller.py
@@ -3,16 +3,16 @@ import lemonator
 class controller:
     def __init__(self, lemonator):
         self.lemonator = lemonator()
-        self.lemonator.lcd.putc("H")
-        self.lemonator.lcd.putc("e")
-        self.lemonator.lcd.putc("l")
-        self.lemonator.lcd.putc("l")
-        self.lemonator.lcd.putc("o")
+        for x in "Hello\n":
+            self.lemonator.lcd.putc(x)
 
     def update(self):
         if "A" == lemonator.keypad.getc() and lemonator.reflex.get():
             lemonator.sirup_valve.set(1)
             lemonator.sirup_pump.set(1)
+            for x in "Pumping syrup\n":
+                self.lemonator.lcd.putc(x)
+
             while(1):
                 if lemonator.distance.read_mm() < 110 or not lemonator.reflex.get():
                     lemonator.sirup_pump.set(0)
@@ -21,9 +21,14 @@ class controller:
 
             lemonator.water_valve.set(1)
             lemonator.water_pump.set(1)
+            for x in "Pumping water\n":
+                self.lemonator.lcd.putc(x)
 
             while(1):
                 if lemonator.distance.read_mm() < 90 or not lemonator.reflex.get():
                     lemonator.water_pump.set(0)
                     lemonator.water_valve.set(0)
                     break
+
+            for x in "Finished!\n":
+                self.lemonator.lcd.putc(x)
