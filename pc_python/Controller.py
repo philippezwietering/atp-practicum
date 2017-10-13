b06b3907d6ba8ac5e6a3604ce0ea7e1a84a99eab
philippezwietering <philippe.zwietering@gmail.com>
2017-10-13 09:26:29 +0200

    Added a controller object and made itself useful

--- pc_python/Controller.py
+++ pc_python/Controller.py
@@ -1,15 +1,8 @@
-# uncompyle6 version 2.12.0
-# Python bytecode 3.5 (3350)
-# Decompiled from: Python 3.5.2 (default, Nov 17 2016, 17:05:23)
-# [GCC 5.4.0 20160609]
-# Embedded file name: .\Controller.py
-# Compiled at: 2017-08-29 16:53:26
-# Size of source mod 2**32: 1554 bytes
 import lemonator
 
 class controller:
     def __init__(self, lemonator):
-        self.lemonator = lemonator
+        self.lemonator = lemonator()
         self.lemonator.lcd.putc("H")
         self.lemonator.lcd.putc("e")
         self.lemonator.lcd.putc("l")
--- pc_python/test.py
+++ pc_python/test.py
@@ -1,11 +1,15 @@
 import lemonator
+import Controller
 import time
 
 print( "Python interface demo running" )
 hw = lemonator.lemonator( 2 )
+contr = Controller.controller(hw)
 led = hw.led_yellow
 while 1:
-   led.set( 1 )
-   time.sleep( 0.5 )
-   led.set( 0 )
-   time.sleep( 0.5 )
+   # led.set( 1 )
+   # time.sleep( 0.5 )
+   # led.set( 0 )
+   # time.sleep( 0.5 )
+
+   contr.update()
