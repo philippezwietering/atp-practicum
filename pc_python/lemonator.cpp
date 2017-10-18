856bc919550cb041022ad775bc92ff951dc28859
Nerinai <david.dm.dejong@student.hu.nl>
2017-10-18 15:25:53 +0200

    test voor phillipe

--- pc_python/lemonator.cpp
+++ pc_python/lemonator.cpp
@@ -1,6 +1,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #include "pybind11/pybind11.h"
+#include "pybind11/embed.h"
 #pragma GCC diagnostic pop
 
 namespace py = pybind11;
