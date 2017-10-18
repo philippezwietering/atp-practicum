0bf1d87ee820092b88ec4de0e5acf2ce58fa63c6
Luuk <luuksteeman@live.nl>
2017-10-18 14:21:44 +0200

    wrote controller tests

--- pc_python/Controllertests.py
+++ pc_python/Controllertests.py
@@ -1,20 +1,11 @@
 import Controller
+import imp
 import unittest
 from ../huibonator/Effector import Effector
 from ../huibonator/Sensor import Sensor, TemperatureSensor, LevelSensor, ColourSensor
 from ../huibonator/Constants import *
-from time import sleep
 from ../huibonator/Simulator import Plant, Simulator
 import ../huibonator/Simproxy
-
-def runtests(cont, plant, amount):
-    if amount < 1:
-        print("More than 0 iterations for proper testing")
-
-    for i in range(amount):
-        cont.update()
-        plant.update()
-
 
 class controllertests(unittest.TestCase):
 
@@ -34,24 +25,55 @@ class controllertests(unittest.TestCase):
         plkeypad.pressKey("A")
         self.controller.update()
 
-        self.assertTrue(self.plant.effectors["pumpB"].isOn())
-        self.assertFalse(self.plant.effectors["valveB"].isOn())
+        self.assertTrue(self.plant._effectors["pumpB"].isOn())
+        self.assertFalse(self.plant._effectors["valveB"].isOn())
 
         #Check to see if controller turned off sirup pump and valve when treshold is reached
         #Then turns on the water valve and pump
         self.plant._vessels['mix'].amount = 500
 
         self.controller.update()
-        self.assertFalse(self.plant.effectors["pumpB"].isOn())
-        self.assertTrue(self.plant.effectors["valveB"].isOn())
+        self.assertFalse(self.plant._effectors["pumpB"].isOn())
+        self.assertTrue(self.plant._effectors["valveB"].isOn())
 
-        self.assertTrue(self.plant.effectors["pumpA"].isOn())
-        self.assertFalse(self.plant.effectors["valveA"].isOn())
+        self.assertTrue(self.plant._effectors["pumpA"].isOn())
+        self.assertFalse(self.plant._effectors["valveA"].isOn())
 
         #Check to see if controller turned off water pump and valve when treshold is reahced
         self.plant._vessels['mix'].amount = 1650
         self.controller.update()
 
-        self.assertFalse(self.plant.effectors["pumpA"].isOn())
-        self.assertTrue(self.plant.effectors["valveA"].isOn())
-        
+        self.assertFalse(self.plant._effectors["pumpA"].isOn())
+        self.assertTrue(self.plant._effectors["valveA"].isOn())
+
+    def testRemoval(self):
+        print("Testing what happens when cup is removed\n")
+        plkeypad = self.plant._sensors["keypad"]
+        plreflex = self.plant._sensors["reflex"]
+
+        plkeypad.pressKey("A")
+        self.controller.update()
+        self.plant._vessels['mix'].amount = 200
+        self.plant._vessels['mix'].toggle()
+        self.controller.update()
+
+        #Cup is removed so sirup pump and valve should be turned off
+        #and state should be "Waiting"
+        self.assertFalse(self.plant._effectors["pumpB"].isOn())
+        self.assertTrue(self.plant._effectors["valveB"].isOn())
+        self.assertTrue(self.controller.state == "Waiting")
+
+        #Same test but now for water pump and valve
+        plkeypad.pressKey("A")
+        self.controller.update()
+        self.plant._vessels['mix'].amount = 1000
+        self.plant._vessels['mix'].toggle()
+        self.controller.update()
+
+        #Cup is removed so water pump and valve should be turned off
+        #and state should be "Waiting"
+        self.assertFalse(self.plant._effectors["pumpA"].isOn())
+        self.assertTrue(self.plant._effectors["valveA"].isOn())
+        self.assertTrue(self.controller.state == "Waiting")
+
+unittest.main()
