097249806a378e7b743b4561cb5faae086b5c258
philippezwietering <philippe.zwietering@gmail.com>
2017-10-18 14:24:53 +0200

    Changed stuff after reading documentation

--- pc_cpp/interface.cpp
+++ pc_cpp/interface.cpp
@@ -6,31 +6,19 @@
 namespace py = pybind11;
 
 py::object py_plant = py::module::import("Simulator").attr("Plant");
-py::object py_led_yellow = py::module::import("Simproxy").attr("led_yellow");
-py::object py_led_green = py::module::import("Simproxy").attr("led_green");
-py::object py_water_pump = py::module::import("Simproxy").attr("water_pump");
-py::object py_sirup_pump = py::module::import("Simproxy").attr("sirup_pump");
-py::object py_heater = py::module::import("Simproxy").attr("heater");
-py::object py_lcd = py::module::import("Simproxy").attr("lcd");
-py::object py_colour = py::module::import("Simproxy").attr("colour");
-py::object py_distance = py::module::import("Simproxy").attr("distance");
-py::object py_reflex = py::module::import("Simproxy").attr("reflex");
-py::object py_keypad = py::module::import("Simproxy").attr("keypad");
+py::object py_lemonator = py::module::import("Simproxy").attr("lemonator");
+py::object py_simulator = py::module::import("Simulator").attr("Simulator");
 
-class Lemonator{
-private:
-	py_plant &plant;
-	py_led_yellow &led_yellow;
-	py_led_green &led_green;
-	py_water_pump &water_pump;
-	py_sirup_pump &sirup_pump;
-	py_heater &heater;
-	py_lcd &lcd;
-	py_colour &colour;
-	py_distance &distance;
-	py_reflex &reflex;
-	py_keypad &keypad;
+// py::object py_led_yellow = py::module::import("Simproxy").attr("led_yellow");
+// py::object py_led_green = py::module::import("Simproxy").attr("led_green");
+// py::object py_water_pump = py::module::import("Simproxy").attr("water_pump");
+// py::object py_sirup_pump = py::module::import("Simproxy").attr("sirup_pump");
+// py::object py_heater = py::module::import("Simproxy").attr("heater");
+// py::object py_lcd = py::module::import("Simproxy").attr("lcd");
+// py::object py_colour = py::module::import("Simproxy").attr("colour");
+// py::object py_distance = py::module::import("Simproxy").attr("distance");
+// py::object py_reflex = py::module::import("Simproxy").attr("reflex");
+// py::object py_keypad = py::module::import("Simproxy").attr("keypad");
 
-public:
-	Lemonator()
-};
+py::object plant = py_plant.attr("__init__")();
+py::object lemonator = py_lemonator.attr("__init__")(plant);
