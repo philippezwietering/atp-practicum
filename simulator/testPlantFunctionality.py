11b5b5c6d11f28a3646daf0c669e384f77bc9165
philipijnos <philie753@gmail.com>
2019-07-16 00:08:24 +0200

    Added new test file

---
+++ simulator/testPlantFunctionality.py
@@ -0,0 +1,8 @@
+# This file unittests the functionality of the plant, so without any usage of any kind of controller
+
+import unittest
+from unittest import TestCase
+from unittest.mock import Mock
+from Sensor import *
+
+# So far not a lot
